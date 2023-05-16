d8ffff8a67dcfe58fbd597ad2cefdb3f9dd075fe
NakamuraTakuto <rulitaku@icloud.com>
2023-05-16 17:51:59 +0900

    [updata] PlayerMove(歩くアニメーションの追加)

--- 2023_OpenHorror/Assets/Script/Player/PlayerMove.cs
+++ 2023_OpenHorror/Assets/Script/Player/PlayerMove.cs
@@ -8,9 +8,11 @@ public class PlayerMove : MonoBehaviour
     [Header("MoveSpeed")]
     [SerializeField] float _moveSpeed = 5f;
     Rigidbody _rb;
+    Animator _anim;
 
     void Start()
     {
+        _anim = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody>();   
     }
 
@@ -20,12 +22,13 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 dir = Vector3.forward * v + Vector3.right * h;
+        float hv = h + v != 0 ? 5f : 0;
 
         if (dir != Vector3.zero)
         {
             gameObject.transform.forward = dir;
         }
-
+        _anim.SetFloat("SpeedF", hv);
         _rb.velocity = dir.normalized * _moveSpeed;
     }
 }
